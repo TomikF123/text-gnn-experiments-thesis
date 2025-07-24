0551b70ed442b694339c0ca19cfac980b54dc30b
TomikF123 <tomas.felcan@icloud.com>
2025-07-24 09:47:58 +0200

    idk

--- loadData.py
+++ loadData.py
@@ -2,12 +2,16 @@ from dataset import TextDataset
 import loaders
 import loaders.lstmLoader as lstmLoader
 from prepData import clean_data
-from utils import get_save_path
-from utils import get_data_path
+from utils import get_data_path, get_saved_path
 import pandas as pd
 import torch
 import os
 from prepData import encode_tokens, encode_labels, encode_dataset
+from loaders.lstmLoader import LSTMDataset
+
+#Create a saved folder at get_saved_path() if it does not exist
+if not os.path.exists(get_saved_path()):
+    os.makedirs(get_saved_path())
 
 def create_file_name(dataset_config: dict, model_type: str) -> str:
     name = dataset_config["name"]
@@ -17,11 +21,12 @@ def create_file_name(dataset_config: dict, model_type: str) -> str:
     preprocess_config = dataset_config["preprocess"]
     remove_stopwords = preprocess_config["remove_stopwords"]
     remove_rare_words = preprocess_config["remove_rare_words"]
+    vocab_size = dataset_config.get("vocab_size", None)
 
     if model_type == "lstm":
-        save_fn = '{}_train_{}_val_{}_test_{}_seed_{}_stop_words_{}_rare_words_{}'.format(name, train_ratio,
+        save_fn = '{}_train_{}_val_{}_test_{}_seed_{}_stop_words_{}_rare_words_{}_vocab_size_{}'.format(name, train_ratio,
                                                               val_ratio, test_ratio,
-                                                              dataset_config["random_seed"],remove_stopwords,remove_rare_words)
+                                                              dataset_config["random_seed"],remove_stopwords,remove_rare_words,vocab_size)
     elif model_type == "text_gcn":
         pass
     elif model_type == "text_level_gnn":
@@ -40,11 +45,12 @@ def load_data(dataset_config: dict, model_type: str) -> dict[str,TextDataset]:
     # preprocess_config = dataset_config["preprocess"]
     # remove_stopwords = preprocess_config["remove_stopwords"]
     # remove_rare_words = preprocess_config["remove_rare_words"]
-    if save_fn not in get_save_path():
-        create_dataset(save_fn,model_type,dataset_config)
+    save_path = os.path.join(get_saved_path(), save_fn)
+    if not os.path.exists(save_path):
+        create_dataset(save_fn = save_fn, model_type=model_type, dataset_config=dataset_config)
         return load_data(dataset_config, model_type)
     else:
-        save_path = os.path.join(get_save_path(), save_fn)
+        #save_path = os.path.join(get_saved_path(), save_fn)
         dataset_dict = {}
 
         train_path = os.path.join(save_path, "train.pt")
@@ -65,24 +71,57 @@ def load_data(dataset_config: dict, model_type: str) -> dict[str,TextDataset]:
 
     return dataset_dict
 
-    
+def get_tensors_tvt_split(tensors: dict, save_path: str,tvt_split: list,seed: int = 42)->dict[str, tuple[torch.Tensor, torch.Tensor]]:
+    #os.makedirs(save_path, exist_ok=True)
+    from sklearn.model_selection import train_test_split
+    X = tensors['X']
+    y = tensors['y']
+    train_ratio = tvt_split[0]
+    val_ratio = tvt_split[1]
+    test_ratio = tvt_split[2]
+    X_train, X_temp, y_train, y_temp = train_test_split(
+        X, y, test_size=(1 - train_ratio), random_state=seed, stratify=y
+    )
+    if val_ratio > 0:
+        X_val, X_test, y_val, y_test = train_test_split(
+            X_temp, y_temp, test_size=(test_ratio / (test_ratio + val_ratio)), random_state=seed, stratify=y_temp
+        )
+    else:
+        X_val, y_val = None, None
+        X_test, y_test = X_temp, y_temp
+    return {"train": (X_train, y_train),
+            "val": (X_val, y_val),
+            "test": (X_test, y_test)}
 
-def create_dataset(dataset_config: dict, model_type: str):
+def create_dataset(dataset_config: dict, model_type: str, save_fn: str):
     if model_type == "lstm":
         name = dataset_config["name"]
-        preprocess_config = dataset_config["preprocessing"]
+        preprocess_config = dataset_config["preprocess"]
+        vocab_size = dataset_config.get("vocab_size", None)
         df= pd.read_csv(os.path.join(get_data_path(), f"{name}.csv"))
-        df = clean_data(df, remove_stop_words=preprocess_config["remove_stopwords"], remove_rare_words=preprocess_config["remove_rare_words"])
-        text_tensors,label_tensors = encode_dataset(df,model_type= model_type, encode_token_type=dataset_config["encoding"]["encode_token_type"])
-
-
-    #if model_type == "lstm":
-    # df = pd.read_csv(get_data_path + "/" + name + ".csv")
-            # df = clean_data(df, remove_stop_words=remove_stopwords, remove_rare_words=remove_rare_words)
-            # dataset = lstmLoader.LSTMDataset(df["text"].values, df["label"].values)
-        #     # torch.save(dataset, get_save_path() + "/" + save_fn + ".pt")
-        #     return lstmLoader.LSTMDataset(df["text"].values, df["label"].values)
-        # elif model_type == "text_gcn":
-        #     pass
-        # else:
-        #     raise ValueError(f"Unsupported model type: {model_type}")
+        df,vocab = clean_data(df, remove_stop_words=preprocess_config["remove_stopwords"], remove_rare_words=preprocess_config["remove_rare_words"],vocab_size=vocab_size) # remove the vocab..
+        X_tensor,y_tensors = encode_dataset(df,model_type= model_type, encode_token_type=dataset_config["encoding"]["encode_token_type"],vocab=vocab)
+        split_dict = get_tensors_tvt_split(tensors={"X": X_tensor, "y": y_tensors},tvt_split= dataset_config["tvt_split"],save_path=save_fn, seed=dataset_config["random_seed"])
+        for split, (X, y) in split_dict.items():
+            dataset = LSTMDataset(data=X, labels=y)
+            save_dir = os.path.join(get_saved_path(), save_fn)
+            os.makedirs(save_dir, exist_ok=True)
+            torch.save(dataset, os.path.join(save_dir, f"{split}.pt"))
+if __name__ == "__main__":
+    # Example usage
+    dataset_config = {
+        "name": "mr",
+        "tvt_split": [0.8,0, 0.2],
+        "random_seed": 42,
+        "vocab_size": 10000,
+        "preprocess": {
+            "remove_stopwords": False,
+            "remove_rare_words": 0
+        },
+        "encoding": {
+            "encode_token_type": "index"
+        }
+    }
+    model_type = "lstm"
+    dataset = load_data(dataset_config, model_type)
+    print(dataset["train"].data) 
--- prepData.py
+++ prepData.py
@@ -8,13 +8,13 @@ import torch
 from collections import Counter
 import torch.nn.functional as F
 
-def clean_data(df:pd.DataFrame, label_col:str='label', text_col:str='text',remove_stop_words:bool=True,remove_rare_words:int=True)-> list[pd.DataFrame,dict]:    
+def clean_data(df:pd.DataFrame, label_col:str='label', text_col:str='text',remove_stop_words:bool=True,remove_rare_words:int=True,vocab_size:int=None)-> list[pd.DataFrame,dict]:    
     df.dropna(subset=[f"{text_col}"], inplace=True)
     df.dropna(subset=[f"{label_col}"], inplace=True)
     df[f"{text_col}"] = df[f"{text_col}"].astype(str)
     df[f"{text_col}"] = df[f"{text_col}"].str.lower()
     df[f"{text_col}"] = clean_doc(df[f"{text_col}"])
-    vocab = create_vocab(df[f"{text_col}"])
+    vocab = create_vocab(df[f"{text_col}"], vocab_size)
     if remove_stop_words:
         df[f"{text_col}"],vocab = stop_words_removal(df[f"{text_col}"], vocab)
     if remove_rare_words:
@@ -23,20 +23,23 @@ def clean_data(df:pd.DataFrame, label_col:str='label', text_col:str='text',remov
 
     return df, vocab
 
-def create_vocab(df:pd.Series)->dict:
+def create_vocab(df:pd.Series,vocab_size:int)->dict:
     from collections import Counter
     vocab = Counter()
     
     #Creatte a frequency vocab
     for text in df:
         vocab.update(text)
-    
+    if vocab_size:
+        vocab = Counter(dict(vocab.most_common(vocab_size-2))) # -2 for <PAD> and <UNK>
     # Add special tokens
     vocab['<PAD>'] = 0  # Padding token
     vocab['<UNK>'] = 1  # Unknown token
 
     return vocab
-
+def build_word_to_index(vocab:dict) -> dict:
+    word_to_index = {word: idx for idx, word in enumerate(vocab.keys())}
+    return word_to_index
 
 def encode_dataset(df:pd.DataFrame, encode_token_type:str,model_type:str,vocab:dict=None):
     if  model_type == "lstm":
@@ -46,6 +49,7 @@ def encode_dataset(df:pd.DataFrame, encode_token_type:str,model_type:str,vocab:d
 def encode_tokens(encode_token_type,vocab,df:pd.Series, max_len:int=None)->torch.Tensor:
     if encode_token_type == "one-hot":
         vocab_size = len(vocab)
+        vocab = build_word_to_index(vocab)
         index_seq = df.apply(lambda x: [vocab.get(word, vocab['<UNK>']) for word in x])
         index_seq = [torch.tensor(seq, dtype=torch.long) for seq in index_seq]
 
@@ -58,6 +62,11 @@ def encode_tokens(encode_token_type,vocab,df:pd.Series, max_len:int=None)->torch
         pass
     elif encode_token_type == "glove":
         pass
+    elif encode_token_type == "bow":
+        pass
+    elif encode_token_type == "ti-idf":
+        pass
+
     elif encode_token_type == "index":
         from torch.nn.utils.rnn import pad_sequence
         tensor = df.apply(lambda x: [vocab.get(word, vocab['<UNK>']) for word in x])
@@ -111,12 +120,13 @@ def rare_words_removal(df:pd.Series, vocab:dict, min_freq:int=2)->list[pd.Series
 
 if __name__ == "__main__":
     mr_all = pd.read_csv(join(get_data_path(), 'mr.csv'))
-    mr_all,vocab = clean_data(mr_all,remove_stop_words=True, remove_rare_words=0)
+    mr_all,vocab = clean_data(mr_all,remove_stop_words=False, remove_rare_words=0,vocab_size=None)
     print(type(mr_all), mr_all.shape, "\n")
-    text_tensor, label_tensor = encode_dataset(df=mr_all, encode_token_type="one-hot", model_type="lstm",vocab=vocab)
+    text_tensor, label_tensor = encode_dataset(df=mr_all, encode_token_type="ti-idf", model_type="lstm",vocab=vocab)
     print("Text tensor shape:", text_tensor.size())
-    print("Label tensor shape:", label_tensor.size())
-    print(text_tensor[5:10], "\n", label_tensor[:5])
+    #print("Label tensor shape:", label_tensor.size())
+    print(text_tensor[0], "\n")
+    #print(build_word_to_index(create_vocab(mr_all["text"], vocab_size=1000)))
 
     #print(mr_all)
 
