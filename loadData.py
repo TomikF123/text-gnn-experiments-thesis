356f7493d492b1e4286d79a8a5631a18b238f42a
TomikF123 <tomas.felcan@icloud.com>
2025-08-30 14:56:20 +0200

    migrated to uv

--- loadData.py
+++ loadData.py
@@ -122,8 +122,9 @@ def get_dataset_object_func(dataset_config: dict, model_type: str):
 # @memory_profiler.profile
 @line_profiler
 def load_data(dataset_config: dict, model_type: str, split: str) -> TextDataset:
+
+    """ Loads or creates and loads the dataset based on the provided configuration and model type."""
     # save_fn = create_file_name(dataset_config, model_type)
-    # print(dataset_config.get("vocab_size"), "############VOCAB#############")
     dataset_dir_name = create_dir_name_based_on_dataset_config(dataset_config)
     dataset_save_path = os.path.join(
         get_saved_path(), dataset_dir_name
---
+++ loaders/__init__.py
@@ -0,0 +1,13 @@
+"""
+A subpackage for data loaders. 
+There are two types of modules,a loader modules (ends with loader) and a helper module (does not end with loader).
+Each loader module helps the load_data module to load data for a specific model type.
+In each loader module, the following functions are expected to be implemented:
+    - create_file_name: creates a unique file name for the dataset based on the dataset configuration and model type.
+    - get_dataset_class: returns the dataset class for the specific model type.
+    - get_dataset_object_func: returns a function that creates and returns the dataset object.
+
+To extend this framework with a model type that is not compatibile with the 
+"""
+
+__all__ = ["tempLoader", "lstmLoader", "text_gcn_loader","fastTextLoader"]
